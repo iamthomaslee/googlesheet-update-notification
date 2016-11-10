cc3556ea07458c84aa60e224f57eb780328911a7
Thomas B. Lee <leez79@gmail.com>
2016-11-10 09:10:24 -0700

    fixed typo

--- onEditColumn.js
+++ onEditColumn.js
@@ -1,11 +1,11 @@
 /*
 * Author: Thomas Lee (leez79@gmail.com)
 * Date: November 10, 2016
-* Function onEditColumn(e)
 */
 
 function onEditColumn(e) {
-  var emailAddress = "your_email@gmail.com";
+  // variables
+  var emailAddress = "leez79@gmail.com";
   var checkDefinition = [
     {
       sheetIndex:1,
@@ -15,11 +15,12 @@ function onEditColumn(e) {
       sheetIndex:3,
       columnIndex: [2]
     }
-  ];
-  
+  ];  
   var activeSheet = SpreadsheetApp.getActiveSpreadsheet();   
   var docName = activeSheet.getName();
   var sheet = e.range.getSheet();
+  
+  // function
   for (var i = 0; i < checkDefinition.length; i++) {
     if (sheet.getIndex() === checkDefinition[i].sheetIndex) {
       var column = e.range.getColumn();  
@@ -28,7 +29,7 @@ function onEditColumn(e) {
           var row = e.range.getRow();
           var newValue = e.range.getValue();
           var mailSubject = "[Notification] Change on "+sheet.getName()+" of "+docName;
-          var mailBody = subject+"\n\n"+"The value of column "+column+", Row "+row+" has changed.\nNew value: "+newValue;
+          var mailBody = mailSubject+"\n\n"+"The value of column "+column+", Row "+row+" has changed.\nNew value: "+newValue;
           MailApp.sendEmail(emailAddress,mailSubject,mailBody);
         }
       }      
